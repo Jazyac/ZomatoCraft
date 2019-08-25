e135f80088d2ff421681b3c139e0d2367368c72d
jazyac <jazy_ac@hotmail.com>
2019-08-25 18:30:54 +1200

    Shows 20 restaurants in Melbourne via a json String on the /homepage

--- modules/zomatomodule/src/ZomatoModule.php
+++ modules/zomatomodule/src/ZomatoModule.php
@@ -7,42 +7,31 @@
  * @link      example.com
  * @copyright Copyright (c) 2019 jazyac
  */
-
 namespace modules\zomatomodule;
-
 use modules\zomatomodule\assetbundles\zomatomodule\ZomatoModuleAsset;
-
 use Craft;
 use craft\events\RegisterTemplateRootsEvent;
 use craft\events\TemplateEvent;
 use craft\i18n\PhpMessageSource;
 use craft\web\View;
-
 use yii\base\Event;
 use yii\base\InvalidConfigException;
 use yii\base\Module;
-
 /**
  * Class ZomatoModule
  *
- * @author    jazyac
- * @package   ZomatoModule
- * @since     1.0.0
  *
  */
 class ZomatoModule extends Module
 {
     // Static Properties
     // =========================================================================
-
     /**
      * @var ZomatoModule
      */
     public static $instance;
-
     // Public Methods
     // =========================================================================
-
     /**
      * @inheritdoc
      */
@@ -50,7 +39,6 @@ class ZomatoModule extends Module
     {
         Craft::setAlias('@modules/zomatomodule', $this->getBasePath());
         $this->controllerNamespace = 'modules\zomatomodule\controllers';
-
         // Translation category
         $i18n = Craft::$app->getI18n();
         /** @noinspection UnSafeIsSetOverArrayInspection */
@@ -63,20 +51,41 @@ class ZomatoModule extends Module
                 'allowOverrides' => true,
             ];
         }
-
         // Base template directory
         Event::on(View::class, View::EVENT_REGISTER_CP_TEMPLATE_ROOTS, function (RegisterTemplateRootsEvent $e) {
             if (is_dir($baseDir = $this->getBasePath().DIRECTORY_SEPARATOR.'templates')) {
                 $e->roots[$this->id] = $baseDir;
             }
         });
-
+        Craft::$app->view->hook('Display-Restaurants', function(array &$context) {
+        // create curl resource 
+            $ch = curl_init(); 
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'user-key: 1a5ca52213f92365977524d7cda0c9cb',//should be an environment variable.
+                'Accept: application/json'
+            ));
+        // set url 
+            curl_setopt($ch, CURLOPT_URL, "https://developers.zomato.com/api/v2.1/search?entity_id=14&entity_type=city&count=20&collection_id=1"); 
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $output = curl_exec($ch); 
+            if (curl_errno($ch)) {
+                die('Couldn\'t send request: ' . curl_error($ch));
+            } else {
+                $resultStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                if ($resultStatus != 200) {
+                    die('Request failed: HTTP status code: ' . $resultStatus);
+                }
+                else{
+                    $context['JSONrestaurant'] = $output;
+                }
+            }
+        // close curl resource to free up system resources 
+            curl_close($ch);  
+        });
         // Set this as the global instance of this module class
         static::setInstance($this);
-
         parent::__construct($id, $parent, $config);
     }
-
     /**
      * @inheritdoc
      */
@@ -84,7 +93,6 @@ class ZomatoModule extends Module
     {
         parent::init();
         self::$instance = $this;
-
         if (Craft::$app->getRequest()->getIsCpRequest()) {
             Event::on(
                 View::class,
@@ -101,7 +109,6 @@ class ZomatoModule extends Module
                 }
             );
         }
-
         Craft::info(
             Craft::t(
                 'zomato-module',
@@ -111,7 +118,6 @@ class ZomatoModule extends Module
             __METHOD__
         );
     }
-
     // Protected Methods
     // =========================================================================
 }
